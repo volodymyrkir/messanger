192d80e633bb33861f064049ba154531544ff97f
volodymyr <zalorderon3331@gmail.com>
2022-07-09 21:08:21 +0300

    no comments

--- messanger_ui/forms/chat.py
+++ messanger_ui/forms/chat.py
@@ -12,41 +12,22 @@ class ScrollableLabel(ScrollView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # ScrollView does not allow us to add more than one widget, so we need to trick it
-        # by creating a layout and placing two widgets inside it
-        # Layout is going to have one collumn and and size_hint_y set to None,
-        # so height wo't default to any size (we are going to set it on our own)
         self.layout = GridLayout(cols=1, size_hint_y=None)
         self.add_widget(self.layout)
 
-        # Now we need two wodgets - Label for chat history and 'artificial' widget below
-        # so we can scroll to it every new message and keep new messages visible
-        # We want to enable markup, so we can set colors for example
         self.chat_history = Label(size_hint_y=None, markup=True)
         self.scroll_to_point = Label()
 
-        # We add them to our layout
         self.layout.add_widget(self.chat_history)
         self.layout.add_widget(self.scroll_to_point)
 
-    # Methos called externally to add new message to the chat history
     def update_chat_history(self, message):
-        # First add new line and message itself
         self.chat_history.text += '\n' + message
 
-        # Set layout height to whatever height of chat history text is + 15 pixels
-        # (adds a bit of space at teh bottom)
-        # Set chat history label to whatever height of chat history text is
-        # Set width of chat history text to 98 of the label width (adds small margins)
         self.layout.height = self.chat_history.texture_size[1] + 15
         self.chat_history.height = self.chat_history.texture_size[1]
         self.chat_history.text_size = (self.chat_history.width * 0.98, None)
 
-        # As we are updating above, text height, so also label and layout height are going to be bigger
-        # than the area we have for this widget. ScrollView is going to add a scroll, but won't
-        # scroll to the botton, nor there is a method that can do that.
-        # That's why we want additional, empty wodget below whole text - just to be able to scroll to it,
-        # so scroll to the bottom of the layout
         self.scroll_to(self.scroll_to_point)
 
 
@@ -56,39 +37,26 @@ class ChatPage(GridLayout):
         self.user_name = user_name
         self.client = client
         self.another_user_id = another_user_id
-        # We are going to use 1 column and 2 rows
         self.cols = 1
         self.rows = 2
 
-        # First row is going to be occupied by our scrollable label
-        # We want it be take 90% of app height
         self.history = ScrollableLabel(height=Window.size[1] * 0.9, size_hint_y=None)
         self.add_widget(self.history)
 
-        # In the second row, we want to have input fields and Send button
-        # Input field should take 80% of window width
-        # We also want to bind button click to send_message method
         self.new_message = TextInput(width=Window.size[0] * 0.8, size_hint_x=None, multiline=False)
         self.send = Button(text="Send")
         self.send.bind(on_press=self.send_message)
 
-        # To be able to add 2 widgets into a layout with just one column, we use additional layout,
-        # add widgets there, then add this layout to main layout as second row
         bottom_line = GridLayout(cols=2)
         bottom_line.add_widget(self.new_message)
         bottom_line.add_widget(self.send)
         self.add_widget(bottom_line)
 
-    # Gets called when either Send button or Enter key is being pressed
-    # (kivy passes button object here as well, but we don;t care about it)
     def send_message(self, _):
-        # Get message text and clear message input field
         message = self.new_message.text
         self.new_message.text = ''
 
-        # If there is any message - add it to chat history and send to the server
         if message:
-            # Our messages - use red color for the name
             self.history.update_chat_history(f'[color=dd2020]{self.user_name}[/color] > {message}')
             # socket_client.send(message)
 
